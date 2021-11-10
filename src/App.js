4f1497689ad05aa6393943ffc0592340df067466
CodeOfVictor <correovictorb@gmail.com>
2021-11-11 00:09:58 +0100

    update

--- src/App.js
+++ src/App.js
@@ -2,28 +2,29 @@ import './App.css';
 import { Calendar, momentLocalizer  } from 'react-big-calendar';
 import "react-big-calendar/lib/css/react-big-calendar.css";
 import moment from 'moment';
+import schedule from './schedule.PNG';
+import { getEvents } from './EventsCalendar';
 
 const locales = {
   "en-US": require("date-fns/locale/en-US")
 }
 
+moment.locale('ko', {
+  week: {
+      dow: 1,
+      doy: 1,
+  },
+});
+
 const localizer = momentLocalizer(moment);
 
-const events = [
-  { title: "ANALISIS Y CALCULO: Ejercicios foro", allDay: true, start: new Date(2021, 10, 15), end: new Date(2021, 10, 15) },
-  { title: "Game Engine Design", allDay: true, start: new Date(2021, 10, 15), end: new Date(2021, 10, 15) },
-  { title: "ANALISIS Y CALCULO: EXAMEN", allDay: true, start: new Date(2021, 10, 18), end: new Date(2021, 10, 18) },
-  { title: "EXAMEN: Computer Graphics", allDay: true, start: new Date(2021, 10, 30), end: new Date(2021, 10, 30) },
-  { title: "FIESTA", allDay: true, start: new Date(2021, 11, 6), end: new Date(2021, 11, 8) },
-  { title: "GAMERGY", allDay: true, start: new Date(2021, 11, 17), end: new Date(2021, 11, 19) },
-  { title: "NAVIDAD", allDay: true, start: new Date(2021, 11, 22), end: new Date(2022, 0, 7) }
-]
-
+const events = getEvents();
 
 function App() {
   return (
     <div className="App">
       <Calendar localizer={localizer} events={events} startAccessor='start' endAccessor='end' style={{height: 500, margin: '50px'}} />
+      <img src={schedule} style={{maxWidth: '100%', flex: 1}} alt="schedule" />
     </div>
   );
 }
---
+++ src/EventsCalendar.js
@@ -0,0 +1,22 @@
+export const getEvents = () => {
+    return [
+        { title: "EXAMEN: Grafica", allDay: true, start: new Date(2022, 0, 31), end: new Date(2022, 0, 31) },
+        { title: "EXAMEN: AyC", allDay: true, start: new Date(2022, 0, 27), end: new Date(2022, 0, 27) },
+        { title: "EXAMEN: Motores", allDay: true, start: new Date(2022, 0, 25), end: new Date(2022, 0, 25) },
+        { title: "EXAMEN: Web", allDay: true, start: new Date(2022, 0, 24), end: new Date(2022, 0, 24) },
+        { title: "Web trabajo grupal", allDay: true, start: new Date(2022, 0, 20), end: new Date(2022, 0, 31) },
+        { title: "EXAMEN: Persona-Computador", allDay: true, start: new Date(2022, 0, 20), end: new Date(2022, 0, 20) },
+        { title: "EXAMEN: Software", allDay: true, start: new Date(2022, 0, 19), end: new Date(2022, 0, 19) },
+        { title: "NAVIDAD", allDay: true, start: new Date(2021, 11, 22), end: new Date(2022, 0, 8) },
+        { title: "GAMERGY", allDay: true, start: new Date(2021, 11, 17), end: new Date(2021, 11, 19) },
+        { title: "Doble clase etica", allDay: true, start: new Date(2021, 11, 17), end: new Date(2021, 11, 17) },
+        { title: "Ing Soft: Group work 1. Research about a topic", allDay: true, start: new Date(2021, 11, 14), end: new Date(2021, 11, 14) },
+        { title: "Doble clase Web", allDay: true, start: new Date(2021, 11, 9), end: new Date(2021, 11, 9) },
+        { title: "FIESTA", allDay: true, start: new Date(2021, 11, 6), end: new Date(2021, 11, 9) },
+        { title: "EXAMEN: Computer Graphics", allDay: true, start: new Date(2021, 10, 30), end: new Date(2021, 10, 30) },
+        { title: "ANALISIS Y CALCULO: EXAMEN", allDay: true, start: new Date(2021, 10, 18), end: new Date(2021, 10, 18) },
+        { title: "Game Engine Design", allDay: true, start: new Date(2021, 10, 15), end: new Date(2021, 10, 15) },
+        { title: "ANALISIS Y CALCULO: Ejercicios foro", allDay: true, start: new Date(2021, 10, 15), end: new Date(2021, 10, 15) },
+        { title: "Ing soft: Se abren fechas 14:00", allDay: true, start: new Date(2021, 10, 11), end: new Date(2021, 10, 11) }
+    ];
+}
