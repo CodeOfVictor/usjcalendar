a76dafab9ddd1f7690efdbce69d24a2fdbdf144e
CodeOfVictor <correovictorb@gmail.com>
2021-12-24 20:52:59 +0100

    new dates

--- src/EventsCalendar.js
+++ src/EventsCalendar.js
@@ -1,15 +1,18 @@
 export const getEvents = () => {
     return [
-        { title: "EXAMEN: Grafica", allDay: true, start: new Date(2022, 0, 31), end: new Date(2022, 0, 31) },
+        { title: "EXAMEN: Grafica", allDay: true, start: new Date(2022, 0, 28), end: new Date(2022, 0, 28) },
         { title: "EXAMEN: AyC", allDay: true, start: new Date(2022, 0, 27), end: new Date(2022, 0, 27) },
+        { title: "EXAMEN: Web", allDay: true, start: new Date(2022, 0, 27), end: new Date(2022, 0, 27) },
         { title: "EXAMEN: Motores", allDay: true, start: new Date(2022, 0, 25), end: new Date(2022, 0, 25) },
         { title: "EXAMEN: Web", allDay: true, start: new Date(2022, 0, 24), end: new Date(2022, 0, 24) },
         { title: "Web trabajo grupal", allDay: true, start: new Date(2022, 0, 20), end: new Date(2022, 0, 31) },
         { title: "FIESTA SAN VALERO", allDay: true, start: new Date(2022, 0, 28), end: new Date(2022, 0, 28) },
-        { title: "EXAMEN: Persona-Computador", allDay: true, start: new Date(2022, 0, 20), end: new Date(2022, 0, 20) },
+        { title: "EXAMEN: Persona-Computador", allDay: true, start: new Date(2022, 0, 21), end: new Date(2022, 0, 21) },
+        { title: "EXAMEN: Legislacion", allDay: true, start: new Date(2022, 0, 21), end: new Date(2022, 0, 21) },
         { title: "EXAMEN: Software", allDay: true, start: new Date(2022, 0, 19), end: new Date(2022, 0, 19) },
         { title: "ETICA: Trabajo grupal", allDay: true, start: new Date(2022, 0, 7), end: new Date(2022, 0, 7) },
         { title: "NAVIDAD", allDay: true, start: new Date(2021, 11, 22), end: new Date(2022, 0, 8) },
+        
         { title: "GAMERGY", allDay: true, start: new Date(2021, 11, 17), end: new Date(2021, 11, 20) },
         { title: "Doble clase etica", allDay: true, start: new Date(2021, 11, 16), end: new Date(2021, 11, 16) },
         { title: "Ing Soft: Group work 1. Research about a topic", allDay: true, start: new Date(2021, 11, 14), end: new Date(2021, 11, 14) },
@@ -20,6 +23,7 @@ export const getEvents = () => {
         { title: "EXAMEN DE INTERFACES", allDay: true, start: new Date(2021, 11, 9), end: new Date(2021, 11, 9) },
         { title: "Doble clase Web", allDay: true, start: new Date(2021, 11, 9), end: new Date(2021, 11, 9) },
         { title: "FIESTA", allDay: true, start: new Date(2021, 11, 6), end: new Date(2021, 11, 9) },
+
         { title: "EXAMEN: Computer Graphics", allDay: true, start: new Date(2021, 10, 30), end: new Date(2021, 10, 30) },
         { title: "EXAMEN WEB", allDay: true, start: new Date(2021, 10, 25), end: new Date(2021, 10, 25) },
         { title: "Web: Presentar", allDay: true, start: new Date(2021, 10, 23), end: new Date(2021, 10, 25) },
