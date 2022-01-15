2c25c5023010067ac8308d086d3cfddb48829a9e
CodeOfVictor <correovictorb@gmail.com>
2022-01-15 13:05:49 +0100

    update calendar

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ const events = getEvents();
 function App() {
   return (
     <div className="App">
-      <center><h2>Actualizado el: 13/01/2022</h2></center>
+      <center><h2>Actualizado el: 15/01/2022</h2></center>
       <center><h3>Cualquier duda: alu.105374@usj.es</h3></center>
       <Calendar localizer={localizer} events={events} startAccessor='start' endAccessor='end' style={{height: 500, margin: '50px'}} />
       <img src={schedule} style={{maxWidth: '100%', flex: 1, display: 'block', marginLeft: 'auto', marginRight: 'auto', width: '50%'}} alt="schedule" />
